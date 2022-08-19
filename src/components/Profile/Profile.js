86ebce7aa9fe36e8f050620fcf333d5f770ddeb2
ghavasi <g.havasi@outlook.com>
2022-08-19 09:59:58 +0100

    Timeline component was created

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,24 @@
+import { Typography } from "@mui/material";
+import BasicTimeline from "../Timeline/BasicTimeline";
+import "./Profile.css";
+
 const Profile = () => {
-  return <h3>Profile</h3>;
+  return (
+    <div className="profile container-shadow">
+      <div className="profile-name">
+        <Typography className="name">Gabor</Typography>
+        <Typography className="title">Junior Software Engineer</Typography>
+      </div>
+      <figure className="profile-image">
+        <img src={require("../../img/gabor.png")} alt="" />
+      </figure>
+      <div className="profile-information">
+        <BasicTimeline />
+        <br />
+        <button>Button</button>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
---
+++ src/components/Timeline/BasicTimeline.js
@@ -0,0 +1,37 @@
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import "./BasicTimeline.css";
+import { Typography } from "@mui/material";
+import WorkIcon from "@mui/icons-material/Work";
+
+export default function BasicTimeline({ title, icon, children }) {
+  return (
+    <Timeline className={"timeline"}>
+      {/* Header */}
+      <TimelineItem className={"timeline-top"}>
+        <TimelineSeparator>
+          <TimelineDot className={"timeline-dot-header"}>{icon}</TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Typography variant="h6" className={"timeline-header"}>
+            {title}
+          </Typography>
+        </TimelineContent>
+      </TimelineItem>
+
+      {children}
+    </Timeline>
+  );
+}
+
+export const BasicTimelineSeparator = () => (
+  <TimelineSeparator className={"separator-padding"}>
+    <TimelineDot className={"timeline-dot"} />
+    <TimelineConnector />
+  </TimelineSeparator>
+);
